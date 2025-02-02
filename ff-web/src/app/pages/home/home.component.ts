3bc02eeb6c759494a17f8e5324264c3d6dbc878e
13nico01 <nico.z130601@gmail.com>
2025-02-03 00:53:35 +0100

    update landing page, implemented footer component

--- ff-web/src/app/pages/home/home.component.ts
+++ ff-web/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
